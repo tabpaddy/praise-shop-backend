6bbf98f162ec1ec5cfa0b5c73f2008f5f01830a8
praisethedeveloper <taborotap@gmail.com>
2025-01-14 23:20:27 +0100

    setting up the environment for resetting password with either token or otp

---
+++ app/Jobs/SendOtpJob.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Mail\SendOtpMail;
+use Illuminate\Support\Facades\Mail;
+
+class SendOtpJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $email;
+    public $otp;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($email, $otp)
+    {
+        //
+        $this->email = $email;
+        $this->otp = $otp;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle()
+    {
+        Mail::to($this->email)->send(new SendOtpMail($this->otp));
+    }
+}
