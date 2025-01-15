8cb652bf72a7da75bf3422595c1f35a944cbdeea
praisethedeveloper <taborotap@gmail.com>
2025-01-15 17:39:01 +0100

    added the forgot password, subscribe, reset password so that the frontend can communicate with the server

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ForgotPasswordController;
+use App\Http\Controllers\SubscribeController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -12,3 +14,6 @@ Route::get('/user', function (Request $request) {
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('/subscribe', [SubscribeController::class, "subscribe"]);
+Route::post('/forgot-password', [ForgotPasswordController::class, 'sendResetLink']);
+Route::post('/reset-password', [ForgotPasswordController::class, 'resetPassword']);
