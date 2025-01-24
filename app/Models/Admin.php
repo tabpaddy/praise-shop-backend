7f73c9439ea988a49bd3b908096182b8e22e00e5
praisethedeveloper <taborotap@gmail.com>
2025-01-24 16:02:42 +0100

    created the admin model for eazy communication with the database

---
+++ app/Models/Admin.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+
+class Admin extends Authenticatable
+{
+    use HasApiTokens, Notifiable;
+
+    protected $fillable = ['name', 'email', 'password', 'subAdmin'];
+
+    protected $hidden = ['password', 'remember_token'];
+}
