fc24a7566a123388316730b5bd0450cac6222374
praisethedeveloper <taborotap@gmail.com>
2025-01-26 23:30:07 +0100

    created middleware for auth admin

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -72,5 +72,6 @@ class Kernel extends \Illuminate\Foundation\Http\Kernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'auth.admin' => \App\Http\Middleware\AuthenticateAdmin::class,
     ];
 }
---
+++ app/Http/Middleware/AuthenticateAdmin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthenticateAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next)
+{
+    $admin = Auth::guard('admin')->user();
+    if (!$admin) {
+        return response()->json(['message' => 'Unauthenticated.'], 401);
+    }
+
+    return $next($request);
+}
+}
