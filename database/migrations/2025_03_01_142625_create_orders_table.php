2319100846e069753e875cd97fa1cb9323907ce6
praisethedeveloper <taborotap@gmail.com>
2025-03-01 15:57:57 +0100

    creating the order table and model with controller

---
+++ database/migrations/2025_03_01_142625_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->decimal('amount', 8, 2);
+            $table->decimal('invoice_no');
+            $table->string('qty');
+            $table->string('size');
+            $table->string('order_status');
+            $table->int('total_products'); // do know if its needed
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
