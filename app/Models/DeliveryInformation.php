4a66ea12c1948189bc6411a82b9216500699fb37
praisethedeveloper <taborotap@gmail.com>
2025-03-01 16:31:29 +0100

    created delivery information migartion and model

---
+++ app/Models/DeliveryInformation.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class DeliveryInformation extends Model
+{
+    //
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'first_name', 'last_name', 'email', 'street', 'city', 'state', 'zip_code', 'country', 'phone'];
+
+    public function user(): BelongsTo{
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
