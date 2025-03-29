69d664a62beea259fc4d71022cd25c4723f1f300
praisethedeveloper <taborotap@gmail.com>
2025-03-29 09:21:51 +0100

    extra dependency for the project

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
+
+class Handler extends ExceptionHandler
+{
+    public function register(): void
+    {
+        // Custom exception handling
+    }
+}
